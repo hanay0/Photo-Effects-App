66e21ebb855b814fe87e1bcd035f6cc01f2d5952
hanafy0 <30327222+hanay0@users.noreply.github.com>
2020-09-24 05:23:34 +0200

    adding a photo into the canvas and assign it's width and height

--- plugin.js
+++ plugin.js
@@ -1,1 +1,12 @@
+const canvas = document.querySelector('#canvas');
+const ctx = canvas.getContext('2d');
 
+// creating a new image
+const canvasImg = new Image();
+canvasImg.src = 'town.jpg';
+
+canvasImg.onload = () => {
+    canvas.width = canvasImg.width;
+    canvas.height = canvasImg.height;
+  ctx.drawImage(canvasImg, 0, 0);  
+}
